902db8d5a3f6def279e8645824f43008a17bd059
Dan Kee <dan@dankee.com>
2013-10-05 23:09:21 -0500

    Fix default page

--- src/clj/robot_kata/server.clj
+++ src/clj/robot_kata/server.clj
@@ -1,10 +1,12 @@
 (ns robot-kata.server
   (:require [ring.adapter.jetty :refer [run-jetty          ]]
+            [ring.util.response :refer [redirect           ]]
             [compojure.core     :refer [defroutes GET      ]]
             [compojure.handler  :refer [site               ]]
             [compojure.route    :refer [resources not-found]] ) )
 
 (defroutes robot-kata-routes
+           (GET "/" [_] (redirect "/robot-kata.html"))
            (resources "/")
            (not-found "Page not found") )
 
