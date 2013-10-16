c069ef65a20c9d04918c87a4f4aeafa7bf6f18a8
Dan Kee <dan@dankee.com>
2013-10-15 21:40:32 -0500

    Add cljs testing framework

--- project.clj
+++ project.clj
@@ -9,14 +9,22 @@
   :min-lein-version "2.1.2"
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :plugins [[lein-cljsbuild "0.3.3"]]
+  :plugins [[lein-cljsbuild                  "0.3.3"]
+            [com.cemerick/clojurescript.test "0.1.0"] ]
   :dependencies [[org.clojure/clojure       "1.5.1"   ]
                  [org.clojure/clojurescript "0.0-1878"]
                  [ring/ring-jetty-adapter   "1.1.6"   ]
                  [compojure                 "1.1.5"   ] ]
+  :hooks [leiningen.cljsbuild]
 
-  :cljsbuild {:builds
-              [{:source-paths ["src/cljs"]
-                :compiler {:output-to "resources/public/robot-kata.js"
-                           :optimizations :whitespace
-                           :pretty-print true } }] } )
+  :cljsbuild {:builds {:dev {:source-paths ["src/cljs"]
+                             :compiler {:output-to "resources/public/robot-kata.js"
+                                        :optimizations :whitespace
+                                        :pretty-print true } }
+                       
+                       :test {:source-paths ["src/cljs" "test/cljs"]
+                              :compiler {:output-to "target/robot-kata-test.js"
+                                         :optimizations :whitespace
+                                         :pretty-print true } } }
+              
+              :test-commands {"phantom-test" ["phantomjs" :runner "target/robot-kata-test.js"]} } )
