1a1a51fff24d70cf0eea157d000073463812d827
Dan Kee <dan@dankee.com>
2013-10-25 21:42:31 +0000

    Recognize color under mouse

--- src/cljs/robot_kata/core.cljs
+++ src/cljs/robot_kata/core.cljs
@@ -3,7 +3,7 @@
             [cljs.core.async       :refer [<! chan put!]]
             [goog.events           :refer [listen      ]]
             [goog.events.EventType                     ])
-  
+
   (:require-macros [cljs.core.async.macros :refer [go]]) )
 
 (defn get-2d-context [canvas]
@@ -14,10 +14,29 @@
     (set! (.-onload image) #(do (.drawImage context image 0 0) (callback)))
     (set! (.-src image) image-name) ) )
 
-(load-image (get-2d-context (by-id "floor")) "roomba-dock.png" #(js/console.log "Robot Kata!"))
+(defn get-event-offset-position [event]
+  {:x (.-offsetX event) :y (.-offsetY event)} ) ; Only works in WebKit????
 
-(let [floor   (by-id "floor")
-      channel (let [c (chan)] (listen floor goog.events.EventType.MOUSEMOVE #(put! c %)) c) ]
-  (go (while true
-        (let [event (<! channel)]
-          (js/console.log (str (.-offsetX event) ", " (.-offsetY event))) ) )) )
+(defn get-pixel-color [context position]
+  (let [image-data (.-data (.getImageData context (position :x) (position :y) 1 1))]
+    {:r (aget image-data 0) :g (aget image-data 1) :b (aget image-data 2)} ) )
+
+(defn get-color-name [color]
+  (let [threshold 192]
+    (cond (and (> (color :r) threshold) (> (color :g) threshold) (> (color :b) threshold)) "white"
+          (and (> (color :r) threshold) (> (color :g) threshold))                          "yellow"
+          (> (color :r) threshold)                                                         "red"
+          (> (color :g) threshold)                                                         "green"
+          (> (color :b) threshold)                                                         "blue"
+          true                                                                             "black" ) ) )
+
+(defn ^:export run []
+  (load-image (get-2d-context (by-id "floor")) "roomba-dock.png" #(js/console.log "Robot Kata!"))
+
+  (let [floor   (by-id "floor")
+        channel (let [c (chan)] (listen floor goog.events.EventType.MOUSEMOVE #(put! c %)) c) ]
+    (go (while true
+          (let [event (<! channel)]
+            (js/console.log (get-color-name (get-pixel-color (get-2d-context (by-id "floor"))
+                                                             (get-event-offset-position event) ))) ) )) ) )
+
--- test/cljs/robot_kata/core_test.cljs
+++ test/cljs/robot_kata/core_test.cljs
@@ -1,10 +1,19 @@
 (ns robot-kata.core-test
   (:require-macros [cemerick.cljs.test :refer (is deftest testing)])
-  (:require        [cemerick.cljs.test                            ]
-                   [robot-kata.core    :refer (get-2d-context)    ] ) )
+  (:require        [cemerick.cljs.test                                       ]
+                   [robot-kata.core    :refer (get-2d-context get-color-name)] ) )
 
 (deftest robot-kata-core-test
 
   (testing "get-2d-context"
     (let [canvas (.createElement js/document "canvas")]
-      (is (instance? js/CanvasRenderingContext2D (get-2d-context canvas)) "Returns CanvasRenderingContext2D") ) ) )
+      (is (instance? js/CanvasRenderingContext2D (get-2d-context canvas)) "returns CanvasRenderingContext2D") ) )
+
+  (testing "get-color-name"
+    (is (= (get-color-name {:r 200 :g 230 :b 193}) "white" ) "detects white correctly" )
+    (is (= (get-color-name {:r 200 :g 230 :b 191}) "yellow") "detects yellow correctly")
+    (is (= (get-color-name {:r 200 :g   0 :b 191}) "red"   ) "detects red correctly"   )
+    (is (= (get-color-name {:r   0 :g 200 :b 191}) "green" ) "detects green correctly" )
+    (is (= (get-color-name {:r 123 :g   0 :b 193}) "blue"  ) "detects blue correctly"  )
+    (is (= (get-color-name {:r   0 :g 100 :b 191}) "black" ) "detects black correctly" ) ) )
+
