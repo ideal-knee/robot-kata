427f43f6da1f32c95c5542efd2f9b51c539d961d
Dan Kee <dan@dankee.com>
2013-10-31 19:01:21 +0000

    Remove ClojureScript repl stuff

--- project.clj
+++ project.clj
@@ -17,17 +17,10 @@
                  [org.clojure/core.async    "0.1.242.0-44b1e3-alpha"]
                  [ring/ring-jetty-adapter   "1.1.6"                 ]
                  [compojure                 "1.1.5"                 ]
-                 [com.cemerick/piggieback   "0.1.0"                 ]
                  [prismatic/dommy           "0.1.3-SNAPSHOT"        ] ]
-  ; :hooks [leiningen.cljsbuild]
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
   :ring {:handler robot-kata.server/handler
          :open-browser? false }
-  :injections [(require '[cljs.repl.browser   :refer [repl-env ]]
-                        '[cemerick.piggieback :refer [cljs-repl]] )
-               (defn browser-repl []
-                 (cljs-repl :repl-env (repl-env :port 8000)) )
-               (println ">>>>>>>>>> To launch browser repl: '(browser-repl)' <<<<<<<<<<<") ]
 
   :cljsbuild {:builds {:dev {:source-paths ["src/cljs"]
                              :compiler {:output-to "resources/public/robot-kata.js"
--- src/cljs/robot_kata/repl.cljs
+++
@@ -1,4 +0,0 @@
-(ns robot-kata.repl
-    (:require [clojure.browser.repl :refer [connect]]) )
-
-(connect "http://localhost:8000/repl")
