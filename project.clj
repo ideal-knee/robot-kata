266e6af39a96260d030d298eb8d43a9682f8ca49
Dan Kee <dan@dankee.com>
2013-11-05 22:00:17 -0600

    Update to point to my snapshot of dommy on Clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject robot-kata "0.1.0-SNAPSHOT"
 
   :description "A web application to explore a simple robot programming problem"
-  :url "http://example.com/FIXME"
+  :url "http://robot-kata.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html" }
 
@@ -9,15 +9,17 @@
   :min-lein-version "2.1.2"
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
+
   :plugins [[lein-cljsbuild                  "0.3.3"]
             [lein-ring                       "0.8.7"]
             [com.cemerick/clojurescript.test "0.1.0"] ]
-  :dependencies [[org.clojure/clojure       "1.5.1"                 ]
-                 [org.clojure/clojurescript "0.0-1878"              ]
-                 [org.clojure/core.async    "0.1.242.0-44b1e3-alpha"]
-                 [ring/ring-jetty-adapter   "1.1.6"                 ]
-                 [compojure                 "1.1.5"                 ]
-                 [prismatic/dommy           "0.1.3-SNAPSHOT"        ] ]
+
+  :dependencies [[org.clojure/clojure          "1.5.1"                 ]
+                 [org.clojure/clojurescript    "0.0-1878"              ]
+                 [org.clojure/core.async       "0.1.242.0-44b1e3-alpha"]
+                 [ring/ring-jetty-adapter      "1.1.6"                 ]
+                 [compojure                    "1.1.5"                 ]
+                 [org.clojars.ideal-knee/dommy "0.1.3-SNAPSHOT"        ] ]
 
   :ring {:handler robot-kata.server/handler
          :open-browser? false }
