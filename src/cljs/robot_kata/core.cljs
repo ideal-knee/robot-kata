6ed819078d93f81855d28e099aa9624daa09ef5f
Dan Kee <dan@dankee.com>
2013-12-06 21:23:36 -0600

    test animation working

--- src/cljs/robot_kata/core.cljs
+++ src/cljs/robot_kata/core.cljs
@@ -3,13 +3,10 @@
   (:require [cljs.core.async       :refer [<! chan put!  ]]
             [goog.events           :refer [listen        ]]
             [goog.events.EventType :refer [MOUSEMOVE     ]]
-            [robot-kata.robot      :refer [get-sensor-position-from-mouse init-robot-svg set-position!]] )
+            [robot-kata.robot      :refer [get-2d-context get-sensor-position-from-mouse init-robot-svg set-position!]] )
 
   (:require-macros [cljs.core.async.macros :refer [go  ]]
                    [dommy.macros           :refer [sel1]] ) )
-
-(defn get-2d-context [canvas]
-  (.getContext canvas "2d") )
 
 (defn load-image [context, image-name, callback]
   (let [image (js/Image.)]
@@ -18,19 +15,6 @@
 
 (defn get-event-offset-position [event]
   {:x (.-offsetX event) :y (.-offsetY event)} ) ; Only works in WebKit????
-
-(defn get-pixel-color [context position]
-  (let [image-data (.-data (.getImageData context (position :x) (position :y) 1 1))]
-    {:r (aget image-data 0) :g (aget image-data 1) :b (aget image-data 2)} ) )
-
-(defn get-color-name [color]
-  (let [threshold 192]
-    (cond (and (> (color :r) threshold) (> (color :g) threshold) (> (color :b) threshold)) "white"
-          (and (> (color :r) threshold) (> (color :g) threshold))                          "yellow"
-          (> (color :r) threshold)                                                         "red"
-          (> (color :g) threshold)                                                         "green"
-          (> (color :b) threshold)                                                         "blue"
-          true                                                                             "black" ) ) )
 
 (defn ^:export run []
   (let [floor-context (get-2d-context (sel1 :#floor))
@@ -42,9 +26,13 @@
 
     (init-robot-svg (sel1 :#robot-svg))
 
+    #_
     (go
      (while true
        (let [event (<! channel)]
          (set-position! (sel1 :#robot) (assoc (get-event-offset-position event) :theta 135))
-         (js/console.log (get-color-name (get-pixel-color floor-context
-                                                          (get-sensor-position-from-mouse (assoc (get-event-offset-position event) :theta 135)) ))) ) ) ) ) )
+         (js/console.log (get-color-name (get-pixel-color
+                                          floor-context
+                                          (get-sensor-position-from-mouse
+                                           (assoc (get-event-offset-position event) :theta 135) ) ))) ) ) ) ) )
+
--- src/cljs/robot_kata/robot.cljs
+++ src/cljs/robot_kata/robot.cljs
@@ -1,8 +1,8 @@
 (ns robot-kata.robot
 
-  (:require [dommy.core :refer [append! set-attr!]])
+  (:require [dommy.core      :refer [append! set-attr!]])
 
-  (:require-macros [dommy.macros :refer [node]]) )
+  (:require-macros [dommy.macros :refer [node sel1]]) )
 
 (def robot-params {:robot-radius              35
                    :center-to-sensor-distance 30
@@ -11,6 +11,22 @@
                    :robot-color               "silver"
                    :bumper-color              "black"
                    :sensor-color              "black" })
+
+(defn get-2d-context [canvas]
+  (.getContext canvas "2d") )
+
+(defn get-color-name [color]
+  (let [threshold 192]
+    (cond (and (> (color :r) threshold) (> (color :g) threshold) (> (color :b) threshold)) "white"
+          (and (> (color :r) threshold) (> (color :g) threshold))                          "yellow"
+          (> (color :r) threshold)                                                         "red"
+          (> (color :g) threshold)                                                         "green"
+          (> (color :b) threshold)                                                         "blue"
+          true                                                                             "black" ) ) )
+
+(defn get-pixel-color [context position]
+  (let [image-data (.-data (.getImageData context (position :x) (position :y) 1 1))]
+    {:r (aget image-data 0) :g (aget image-data 1) :b (aget image-data 2)} ) )
 
 (defn make-robot-graphic [params]
   (node [:g#robot
@@ -37,6 +53,10 @@
 (defn init-robot-svg [svg]
   (let [robot (make-robot-graphic robot-params)]
     (append! svg robot)
+
+    (animate {:x 0 :y 0 :theta 135})
+
+    #_
     (set-position! robot {:x 400 :y 400 :theta 30}) ) )
 
 (defn calculate-next-position [position distance theta]
@@ -49,7 +69,12 @@
 (defn get-sensor-position [robot-position robot-params]
   (calculate-next-position robot-position (robot-params :center-to-sensor-distance) 0) )
 
-; ???? For testing, remove
-(defn get-sensor-position-from-mouse [mouse-position]
-  (get-sensor-position mouse-position robot-params) )
+(defn animate [robot-position]
+  (js/setTimeout #(let [new-robot-position (calculate-next-position robot-position 5 0)]
+                    (set-position! (sel1 :#robot) new-robot-position)
+                    (js/console.log (get-color-name (get-pixel-color (get-2d-context (sel1 :#floor))
+                                                                     (get-sensor-position new-robot-position
+                                                                                          robot-params ) )))
+                    (animate new-robot-position) )
+                 200 ) )
 
