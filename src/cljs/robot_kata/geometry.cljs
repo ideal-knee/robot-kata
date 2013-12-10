f856552c1cb049c50beb36ff0628e6c618ef183f
Dan Kee <dan@dankee.com>
2013-12-10 19:47:41 +0000

    Improve names of velocity components

--- src/cljs/robot_kata/geometry.cljs
+++ src/cljs/robot_kata/geometry.cljs
@@ -1,17 +1,17 @@
 (ns robot-kata.geometry)
 
 (defn calculate-next-position [position delta]
-  (let [new-theta-degrees (+ (position :theta) (delta :theta))
+  (let [new-theta-degrees (+ (position :theta) (delta :rot))
         new-theta (* new-theta-degrees (/ js/Math.PI 180)) ]
-    {:x (+ (position :x) (* (delta :distance) (js/Math.sin new-theta)))
-     :y (+ (position :y) (- (* (delta :distance) (js/Math.cos new-theta))))
+    {:x (+ (position :x) (* (delta :trans) (js/Math.sin new-theta)))
+     :y (+ (position :y) (- (* (delta :trans) (js/Math.cos new-theta))))
      :theta new-theta-degrees } ) )
 
 (defn get-sensor-position [robot-position robot-params]
-  (calculate-next-position robot-position (robot-params :center-to-sensor-distance) 0) )
+  (calculate-next-position robot-position {:trans (robot-params :center-to-sensor-distance) :rot 0}) )
 
 (defn calculate-position-delta [velocity elapsed-time]
   (let [factor (/ elapsed-time 1000.0)]
-    {:distance (* (velocity :distance) factor)
-     :theta    (* (velocity :theta)    factor) } ) )
+    {:trans (* (velocity :trans) factor)
+     :rot   (* (velocity :rot  ) factor) } ) )
 
--- test/cljs/robot_kata/geometry_test.cljs
+++ test/cljs/robot_kata/geometry_test.cljs
@@ -14,28 +14,28 @@
 
 (deftest robot-kata-geometry-test
   (testing "calculate-next-position"
-    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 0} {:distance 10 :theta 0})]
+    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 0} {:trans 10 :rot 0})]
       (is (approx? (new-position :x)       0) "moves correctly up y axis")
       (is (approx? (new-position :y)     -10) "moves correctly up y axis")
       (is (approx? (new-position :theta)   0) "moves correctly up y axis") )
 
-    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 90} {:distance 10 :theta 0})]
+    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 90} {:trans 10 :rot 0})]
       (is (approx? (new-position :x)      10) "moves correctly right on x axis")
       (is (approx? (new-position :y)       0) "moves correctly right on x axis")
       (is (approx? (new-position :theta)  90) "moves correctly right on x axis") )
 
-    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 0} {:distance 10 :theta 90})]
+    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 0} {:trans 10 :rot 90})]
       (is (approx? (new-position :x)      10) "turns and moves correctly right on x axis")
       (is (approx? (new-position :y)       0) "turns and moves correctly right on x axis")
       (is (approx? (new-position :theta)  90) "turns and moves correctly right on x axis") )
 
-    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 180} {:distance 10 :theta 45})]
+    (let [new-position (calculate-next-position {:x 0 :y 0 :theta 180} {:trans 10 :rot 45})]
       (is (approx? (new-position :x)      (- (js/Math.sqrt 50))) "turns and moves correctly at an angle")
       (is (approx? (new-position :y)          (js/Math.sqrt 50)) "turns and moves correctly at an angle")
       (is (approx? (new-position :theta)                    225) "turns and moves correctly at an angle") ) )
 
   (testing "calculate-position-delta"
-    (let [position-delta (calculate-position-delta {:distance 100 :theta 100} 500)]
-      (is (approx? (position-delta :distance) 50) "calculates position delta based on elapsed time")
-      (is (approx? (position-delta :theta   ) 50) "calculates position delta based on elapsed time")) ) )
+    (let [position-delta (calculate-position-delta {:trans 100 :rot 100} 500)]
+      (is (approx? (position-delta :trans) 50) "calculates position delta based on elapsed time")
+      (is (approx? (position-delta :rot  ) 50) "calculates position delta based on elapsed time")) ) )
 
