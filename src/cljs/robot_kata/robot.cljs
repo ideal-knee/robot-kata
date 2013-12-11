969719fb072155a63d814d4dfa2098c9e21496a4
Dan Kee <dan@dankee.com>
2013-12-10 20:35:00 -0600

    Simulation working

--- src/cljs/robot_kata/robot.cljs
+++ src/cljs/robot_kata/robot.cljs
@@ -1,9 +1,9 @@
 (ns robot-kata.robot
 
-  (:require [dommy.core          :refer [append!                                      ]]
-            [robot-kata.geometry :refer [calculate-next-position get-sensor-position  ]]
-            [robot-kata.image    :refer [get-2d-context get-color-name get-pixel-color]]
-            [robot-kata.svg      :refer [make-robot-graphic set-position!             ]] )
+  (:require [dommy.core          :refer [append!                                                             ]]
+            [robot-kata.geometry :refer [calculate-next-position calculate-position-delta get-sensor-position]]
+            [robot-kata.image    :refer [get-2d-context get-color-name get-pixel-color                       ]]
+            [robot-kata.svg      :refer [make-robot-graphic set-position!                                    ]] )
 
   (:require-macros [dommy.macros :refer [sel1]]) )
 
@@ -17,39 +17,32 @@
 
 (defn init-robot-svg [svg]
   (append! svg (make-robot-graphic robot-params))
-  #_(animate {:x 0 :y 0 :theta 135})
-  #_(animate-interval 1000 #(js/console.log "hi"))
-  (simulate {:x 400 :y 800 :theta 0} {:distance 0 :theta 0} (.now js/Date)) )
+  (simulate {:x 400 :y 800 :theta 0} {:trans 0 :rot 0} (.now js/Date)) )
 
-(defn animate [robot-position]
-  (js/setTimeout #(let [new-robot-position (calculate-next-position robot-position 5 0)]
-                    (set-position! (sel1 :#robot) new-robot-position)
-                    (js/console.log (get-color-name (get-pixel-color (get-2d-context (sel1 :#floor))
-                                                                     (get-sensor-position new-robot-position
-                                                                                          robot-params ) )))
-                    (animate new-robot-position) )
-                 200 ) )
+(let [last-tick-time (atom 0)]
+  (defn time-for-control-tick? []
+    (let [current-time (.now js/Date)]
+      (if (> (- current-time @last-tick-time))
+        (swap! last-tick-time (fn [_] current-time)) ) ) ) )
 
-(defn animate-interval
-  ([period func] (animate-interval period func 0))
-  ([period func last-tick-time]
-     (js/requestAnimationFrame #(let [tick-time (.now js/Date)]
-                                  (if (> (- tick-time last-tick-time) period)
-                                    (do
-                                      (func)
-                                      (animate-interval period func tick-time) )
-                                    (animate-interval period func last-tick-time) ) )) ) )
+(defn get-new-robot-velocity [robot-position]
+  (let [color (get-color-name (get-pixel-color (get-2d-context (sel1 :#floor))
+                                               (get-sensor-position robot-position robot-params) ))]
+    (cond (= color "white") {:trans 50 :rot  0}
+          :else             {:trans  0 :rot 10} ) ) )
 
-(defn simulate [robot-position robot-velocity last-tick-time]
-  (let [tick-time          (.now js/Date)
-        elapsed-time       (min (- tick-time last-tick-time) 100)
-        new-robot-position (calculate-next-position robot-position
-                                                    (calculate-position-delta robot-velocity elapsed-time) ) ]
-    (set-position! (sel1 :#robot) new-robot-position)
+(defn continue-simulation? [] true)
+
+(defn simulate [previous-robot-position robot-velocity last-tick-time]
+  (let [tick-time      (.now js/Date)
+        elapsed-time   (min (- tick-time last-tick-time) 100)
+        robot-position (calculate-next-position previous-robot-position
+                                                (calculate-position-delta robot-velocity elapsed-time) ) ]
+    (set-position! (sel1 :#robot) robot-position)
     (if (continue-simulation?)
-      (js/requestAnimationFrame #(simulate new-robot-position
+      (js/requestAnimationFrame #(simulate robot-position
                                            (if (time-for-control-tick?)
-                                             (get-new-robot-velocity new-robot-position)
+                                             (get-new-robot-velocity robot-position)
                                              robot-velocity )
                                            tick-time ))  ) ) )
 
